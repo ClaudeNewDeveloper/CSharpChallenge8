886e054d1454d068a3311c04b9dc62de9d1a1f10
ClaudeNewDeveloper <besthon@gmail.com>
2016-08-05 01:35:58 -0700

    first commit

---
+++ hackerranktest8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerranktest8
+{
+    class Program
+    {
+        static Dictionary<string, int> phoneBook = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+
+            char[] delimiterChars = {' '};
+            Console.WriteLine("Please, enter the number of contacts in the phone book");
+      
+            string numberOfContacts = Console.ReadLine();
+            int numberOfContactsInPhoneBook = Convert.ToInt32(numberOfContacts);
+
+            for (int i = 0; i < numberOfContactsInPhoneBook; i++)
+            {
+                string userTextInput = Console.ReadLine();
+                string[] nameAndPhoneNumber = userTextInput.Split(delimiterChars);
+                string name = nameAndPhoneNumber[0];
+                string phone = nameAndPhoneNumber[1];
+                int phoneNumber = Convert.ToInt32(phone);
+
+                phoneBook.Add(name, phoneNumber);
+
+            }
+
+            string nameToSearch;
+
+            do
+            {
+                nameToSearch = Console.ReadLine();
+
+                if (nameToSearch != null)
+                {
+                    if (phoneBook.ContainsKey(nameToSearch))
+                    {
+                        int phoneNumberassociated = phoneBook[nameToSearch];
+                        Console.WriteLine(nameToSearch + " = " + phoneNumberassociated);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+
+            }
+            while (nameToSearch != null);
+
+            Console.ReadKey();
+        }
+
+    }
+}
